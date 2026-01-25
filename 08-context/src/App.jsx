d5fd3e54eb6882426bbc6b9c98ecc638c7ac1409
sel-hasn <fangyuansamach@gmail.com>
2026-01-26 00:03:48 +0100

    learning context, and use it in two component Order and Header

---
+++ 08-context/src/App.jsx
@@ -0,0 +1,25 @@
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import PizzaOfTheDay from "./PizzaOfTheDay";
+import Order from "./Order";
+import Header from "./Header";
+import { CartContext } from "./contexts";
+
+const App = () => {
+  const cartHook = useState([]);
+  return (
+    <StrictMode>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
+    </StrictMode>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
