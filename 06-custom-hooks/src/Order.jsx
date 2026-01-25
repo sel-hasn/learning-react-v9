74958b1b71a514598d8e57a7347f42022a8d8c33
sel-hasn <fangyuansamach@gmail.com>
2026-01-25 21:25:42 +0100

    starting with custom hooks usePizzaOfTheDay()

---
+++ 06-custom-hooks/src/Order.jsx
@@ -0,0 +1,123 @@
+import Pizza from "./Pizza";
+import { useEffect, useState } from "react";
+import Cart from "./Cart";
+
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export default function Order() {
+  // const pizzaType = 'pepperoni';
+  // const pizzaSize = 'M';
+
+  const [pizzaType, setPizzaType] = useState("pepperoni");
+  const [pizzaSize, setPizzaSize] = useState("M");
+  const [pizzaTypes, setPizzaTypes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  // console.log('pizzaType:', pizzaType);
+  // console.log('pizzaSize:', pizzaSize);
+
+  let price, selectedPizza;
+
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+    price = intl.format(selectedPizza.sizes[pizzaSize]);
+  }
+
+  async function fetchPizzaTypes() {
+    // await new Promise((resolve) => setTimeout(resolve, 3000)); // remove this later, just to show you the loading state
+
+    const pizzasRes = await fetch("/api/pizzas");
+    const pizzasJson = await pizzasRes.json();
+    setPizzaTypes(pizzasJson);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []);
+
+  return (
+    <div className="order">
+      <h2>Creat Order</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setCart([...cart, { pizza: selectedPizza, size: pizzaSize }, price]);
+        }}
+      >
+        <div>
+          <div>
+            <label htmlFor="pizza-type">Pizza Type</label>
+            <select
+              name="pizza-type"
+              onChange={(event) => setPizzaType(event.target.value)}
+              value={pizzaType}
+            >
+              {pizzaTypes.map((pizza) => (
+                <option key={pizza.id} value={pizza.id}>
+                  {pizza.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pizza-size">Pizza Size</label>
+            <div>
+              <span>
+                <input
+                  checked={pizzaSize === "S"}
+                  type="radio"
+                  name="pizza-size"
+                  value="S"
+                  id="pizza-s"
+                  onChange={(event) => setPizzaSize(event.target.value)}
+                />
+                <label htmlFor="pizza-s">Small</label>
+              </span>
+              <span>
+                <input
+                  checked={pizzaSize === "M"}
+                  type="radio"
+                  name="pizza-size"
+                  value="M"
+                  id="pizza-m"
+                  onChange={(event) => setPizzaSize(event.target.value)}
+                />
+                <label htmlFor="pizza-m">Medium</label>
+              </span>
+              <span>
+                <input
+                  checked={pizzaSize === "L"}
+                  type="radio"
+                  name="pizza-size"
+                  value="L"
+                  id="pizza-l"
+                  onChange={(event) => setPizzaSize(event.target.value)}
+                />
+                <label htmlFor="pizza-l">Large</label>
+              </span>
+            </div>
+          </div>
+          <button type="submit">Add to Cart</button>
+        </div>
+        {loading ? (
+          <h1>loding pizza kanden</h1>
+        ) : (
+          <div className="order-pizza">
+            <Pizza
+              name={selectedPizza.name}
+              description={selectedPizza.description}
+              image={selectedPizza.image}
+            />
+            <p>{price}</p>
+          </div>
+        )}
+      </form>
+      {loading ? <h2>LOADING ...</h2> : <Cart cart={cart} />}
+    </div>
+  );
+}
---
+++ 06-custom-hooks/src/usePizzaOfTheDay.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    pizzaOfTheDay
+      ? `${pizzaOfTheDay.id} : ${pizzaOfTheDay.name}`
+      : "loading ...",
+  );
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const res = await fetch("/api/pizza-of-the-day");
+      const data = await res.json();
+
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
