48a16ae39bed1bacd86aa98a8736a8aaebd47c06
sel-hasn <fangyuansamach@gmail.com>
2026-01-28 20:34:49 +0100

    Integrate Error Boundary with order page

---
+++ 12-error-boundaries/src/Modal.jsx
@@ -0,0 +1,21 @@
+import { Children, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modlRoot = document.getElementById("modal");
+    modlRoot.appendChild(elRef.current);
+    // return "TODO";
+    return () => modlRoot.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
